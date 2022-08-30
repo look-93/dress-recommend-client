da0b0b63fc553dba954c71b2f078e1be4959c353
look-93 <qhfk0961@gmail.com>
2022-08-30 18:29:52 +0900

    로그인 수정

--- src/layout/reviewPage/review.js
+++ src/layout/reviewPage/review.js
@@ -13,7 +13,60 @@ import { Stack } from '@mui/system';
 
 export default function Review() {
   return (
-    <Grid display="flex" padding={6} sx={{ gap: 6 }}>
+    //display="flex" padding={6} sx={{ gap: 6 }}
+    <Grid display="flex" sx={{ justifyContent: 'space-between' }}>
+      <Card>
+        <CardHeader title="id: " subheader="September 14, 2016" />
+        <CardMedia
+          sx={{ width: 250 }}
+          component="img"
+          height="300"
+          image="https://source.unsplash.com/random"
+          alt="unsplash image"
+        />
+        <IconButton>
+          <FavoriteBorderIcon />
+        </IconButton>
+      </Card>
+      <Card>
+        <CardHeader title="id: " subheader="September 14, 2016" />
+        <CardMedia
+          sx={{ width: 250 }}
+          component="img"
+          height="300"
+          image="https://source.unsplash.com/random"
+          alt="unsplash image"
+        />
+        <IconButton>
+          <FavoriteBorderIcon />
+        </IconButton>
+      </Card>
+      <Card>
+        <CardHeader title="id: " subheader="September 14, 2016" />
+        <CardMedia
+          sx={{ width: 250 }}
+          component="img"
+          height="300"
+          image="https://source.unsplash.com/random"
+          alt="unsplash image"
+        />
+        <IconButton>
+          <FavoriteBorderIcon />
+        </IconButton>
+      </Card>
+      <Card>
+        <CardHeader title="id: " subheader="September 14, 2016" />
+        <CardMedia
+          sx={{ width: 250 }}
+          component="img"
+          height="300"
+          image="https://source.unsplash.com/random"
+          alt="unsplash image"
+        />
+        <IconButton>
+          <FavoriteBorderIcon />
+        </IconButton>
+      </Card>
       <Card>
         <CardHeader title="id: " subheader="September 14, 2016" />
         <CardMedia
